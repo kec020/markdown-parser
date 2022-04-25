b145c90e0df2e10d78af1820d0a4f3f6fbd2ff1d
kec020 <kec020@ucsd.edu>
2022-04-24 17:01:26 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,25 +11,14 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
-        while(currentIndex < markdown.length() 
-        && markdown.substring(currentIndex, markdown.length()-1).contains("[")
-        && markdown.substring(currentIndex, markdown.length()-1).contains("]")
-        && markdown.substring(currentIndex, markdown.length()-1).contains("(")
-        && markdown.substring(currentIndex, markdown.length()-1).contains(")")
-        ) {
+        while(currentIndex < markdown.length()) {
             System.out.println(currentIndex);
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            if (openParen == closeParen - 1 
-            || markdown.indexOf("![", currentIndex) == openBracket - 1) {
-                currentIndex = closeParen + 1;
-            }
-            else {
-                toReturn.add(markdown.substring(openParen + 1, closeParen));
-                currentIndex = closeParen + 1;
-            }
+            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            currentIndex = closeParen + 1;
         }
         System.out.println(currentIndex);
 
