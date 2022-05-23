fb0305d648b8b9cc24ffb01ccb1741e2ed20c843
kec020 <kec020@ucsd.edu>
2022-05-23 00:24:58 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -19,20 +19,20 @@ public class MarkdownParseTest {
     public void testSnippet1() throws IOException {
         String contents = Files.readString(Path.of("Snippet 1.md"));
         List<String> expect = List.of("`google.com", "google.com", "ucsd.edu");
-        assertEquals(MarkdownParse.getLinks(contents), expect);
+        assertEquals(expect, MarkdownParse.getLinks(contents));
     }
     
     @Test
     public void testSnippet2() throws IOException {
         String contents = Files.readString(Path.of("Snippet 2.md"));
         List<String> expect = List.of("a.com", "a.com(())", "example.com");
-        assertEquals(MarkdownParse.getLinks(contents), expect);
+        assertEquals(expect, MarkdownParse.getLinks(contents));
     }
 
     @Test
     public void testSnippet3() throws IOException {
         String contents = Files.readString(Path.of("Snippet 3.md"));
         List<String> expect = List.of("https://www.twitter.com", "https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule", "https://cse.ucsd.edu/");
-        assertEquals(MarkdownParse.getLinks(contents), expect);
+        assertEquals(expect, MarkdownParse.getLinks(contents));
     }
 }
